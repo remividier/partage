61d692abc80bc04c54cf667b5f2b7fd6d8a2312e
Eklektik <rvidier.iris09@gmail.com>
2015-04-26 20:07:06 +0200

    sd

--- partage/settings.py
+++ partage/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -100,6 +100,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
--- partage/urls.py
+++ partage/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'backoffice.views.home', name='home'),
 ]
+
